6df38740b5c3a985f441badf9c34f7cec8062680
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 19:42:06 +0100

    better symbol

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@
 #define tp template
 #define sc static const
 #define F struct
-#define C char
+#define Q char
 
 enum class Md {Encode,Decode};
-tp <C ... c> F S {};
+tp <Q ... c> F S {};
 tp<tn T> using V = tn T::v;
 tp <tn T> F Op {};
-tp <C x, C ... y> F Op<S<x, y...>> {sc C f = x; td S<y...> r; td S<y..., x> ro;};
-tp <tn T, C x> F Ad {};
-tp <C x, C ... y> F Ad<S<y...>, x> {td S<y..., x> v;};
-tp <C x, tn T> F Cn {};
-tp <C x, C ... y> F Cn<x, S<y...>> {td S<x, y...> v;};
-tp <C a, C b> F Rg {td tn Cn<a, tn Rg<a+1, b>::v>::v v;};
-tp <C x> F Rg<x,x> {td S<x> v;};
+tp <Q x, Q ... y> F Op<S<x, y...>> {sc Q f = x; td S<y...> r; td S<y..., x> ro;};
+tp <tn T, Q x> F Ad {};
+tp <Q x, Q ... y> F Ad<S<y...>, x> {td S<y..., x> v;};
+tp <Q x, tn T> F Cn {};
+tp <Q x, Q ... y> F Cn<x, S<y...>> {td S<x, y...> v;};
+tp <Q a, Q b> F Rg {td tn Cn<a, tn Rg<a+1, b>::v>::v v;};
+tp <Q x> F Rg<x,x> {td S<x> v;};
 tp <tn A, tn B> F Ct;
 tp <tn A, tn B> F Ct {
   td Op<B> b; 
@@ -25,19 +25,19 @@ tp <tn A, tn B> F Ct {
 };
 tp <tn A> F Ct<A, S<>> {td A v;};
 tp <tn A, tn B> using ct = tn Ct<A, B>::v;
-tp <C x, C y> using rg = tn Rg<x, y>::v;
+tp <Q x, Q y> using rg = tn Rg<x, y>::v;
 td ct<ct<rg<'A', 'Z'>, rg<'0', '9'>>, S<' ', ',', '.'>> CSet;
 tp <tn T> F L {sc int v = 1 + L<tn Op<T>::r>::v;};
 tp <> F L<S<>> {sc int v = 0;};
 constexpr int mup(int x, int y) {return x<0? mup(x+y,y):x;}
-tp <int n, tn T> F Nth {sc C v =  Nth<mup(n-1,L<T>::v), tn Op<T>::ro>::v;};
-tp <tn T> F Nth<0, T> {sc C v = Op<T>::f;};
-tp <C x, tn T> F Fd {};
-tp <C x, C y, C ... z> F Fd<x, S<y, z...>> {sc int v = 1+Fd<x, S<z...>>::v;};
-tp <C x, C ... y> F Fd<x,S<x,y...>> {sc int v = 0;};
-tp <C x> F Ind {sc int v = Fd<x, CSet>::v;};
+tp <int n, tn T> F Nth {sc Q v =  Nth<mup(n-1,L<T>::v), tn Op<T>::ro>::v;};
+tp <tn T> F Nth<0, T> {sc Q v = Op<T>::f;};
+tp <Q x, tn T> F Fd {};
+tp <Q x, Q y, Q ... z> F Fd<x, S<y, z...>> {sc int v = 1+Fd<x, S<z...>>::v;};
+tp <Q x, Q ... y> F Fd<x,S<x,y...>> {sc int v = 0;};
+tp <Q x> F Ind {sc int v = Fd<x, CSet>::v;};
 constexpr int sg(Md m) {return m==Md::Encode? 1 : -1;}
-tp <Md m, C x, C y> F Cd {sc C v = Nth<sg(m)*Ind<x>::v + Ind<y>::v, CSet>::v;};
+tp <Md m, Q x, Q y> F Cd {sc Q v = Nth<sg(m)*Ind<x>::v + Ind<y>::v, CSet>::v;};
 
 tp <Md m, tn K, tn X> F Tr;
 tp <Md m, tn K, tn X> F Tr {
