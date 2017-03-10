d97ccc25a170fee1250fd7d9cce3ea6279f44745
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 13:20:51 +0100

    asdf

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ template <typename T> struct Ops {};
 template <char x, char ... y> struct Ops<String<x, y...>> {static const char first = x; typedef String<y...> rest; typedef String<y..., x> rotated;};
 template <typename T, char x> struct Add {};
 template <char x, char ... y> struct Add<String<y...>, x> {typedef String<y..., x> value;};
-
+template <char x, typename T> struct Cons {};
+template <char x, char ... y> struct Cons<x, String<y...>> {typedef String<x, y...> value;};
 
 
 int main() {
