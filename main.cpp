336409e84deb46326a9803dedb8d860b5e511833
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 13:23:35 +0100

    range good

--- main.cpp
+++ main.cpp
@@ -10,9 +10,10 @@ template <char x, char ... y> struct Cons<x, String<y...>> {typedef String<x, y.
 template <char a, char b> struct Range {typedef typename Cons<a, typename Range<a+1, b>::value>::value value;};
 template <char x> struct Range<x,x> {typedef String<x> value;};
 
-
 int main() {
   Ops<String<'a', 'b'>> x;
   std::cout << "Bra!" << std::endl;
   return 0;
 }
+
+static_assert(std::is_same<String<'a', 'b', 'c'>, typename Range<'a', 'c'>::value>::value, "asdf");
