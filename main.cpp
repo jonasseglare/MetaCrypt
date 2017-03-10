47335321a6eaad7ae161ec8d55bf5999921a1ad2
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 14:17:12 +0100

    asdf

--- main.cpp
+++ main.cpp
@@ -24,6 +24,8 @@ template <char x, char y> using range = tn Range<x, y>::value;
 typedef cat<String<' ', ',', '.'>, cat<range<'A', 'Z'>, range<'0', '9'>>> CharSet;
 template <int n, typename T> struct Nth {static const char value =  Nth<n-1, tn Ops<T>::rotated>::value;};
 template <typename T> struct Nth<0, T> {static const char value = Ops<T>::first;};
+template <typename T> struct Length {static const int value = 1 + Length<tn Ops<T>::rest>::value;};
+template <> struct Length<String<>> {static const int value = 0;};
 
 int main() {
   Ops<String<'a', 'b'>> x;
@@ -31,6 +33,7 @@ int main() {
   return 0;
 }
 
+//static_assert(3 == Length<
 static_assert('c' == Nth<2, String<'a', 'b', 'c', 'd'> >::value, "asdf");
 static_assert(std::is_same<String<'a', 'b', 'c'>, typename Range<'a', 'c'>::value>::value, "asdf");
 static_assert(std::is_same<tn Add<String<'a'>, 'b'>::value, String<'a', 'b'>>::value, "asdf");
