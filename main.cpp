6787ebfd7861b45487591197133988b669bc15b0
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 13:35:01 +0100

    cat

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#define tn typename
+#define td typedef
+#define tp template
+
 template <char ... c> struct String {};
 template <typename T> struct Ops {};
 template <char x, char ... y> struct Ops<String<x, y...>> {static const char first = x; typedef String<y...> rest; typedef String<y..., x> rotated;};
@@ -9,6 +13,7 @@ template <char x, typename T> struct Cons {};
 template <char x, char ... y> struct Cons<x, String<y...>> {typedef String<x, y...> value;};
 template <char a, char b> struct Range {typedef typename Cons<a, typename Range<a+1, b>::value>::value value;};
 template <char x> struct Range<x,x> {typedef String<x> value;};
+template <typename A, typename B> struct Cat {td Ops<B> b;};
 
 int main() {
   Ops<String<'a', 'b'>> x;
@@ -17,3 +22,4 @@ int main() {
 }
 
 static_assert(std::is_same<String<'a', 'b', 'c'>, typename Range<'a', 'c'>::value>::value, "asdf");
+static_assert(std::is_same<tn Add<String<'a'>, 'b'>::value, String<'a', 'b'>>::value, "asdf");
