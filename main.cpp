36463a015b8b79c7f3918d58547d580b44bf40af
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 13:17:03 +0100

    add instead of cons

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 template <char ... c> struct String {};
 template <typename T> struct Ops {};
 template <char x, char ... y> struct Ops<String<x, y...>> {static const char first = x; typedef String<y...> rest; typedef String<y..., x> rotated;};
-template <char x, typename T> struct Cons {};
-template <char x, char ... y> struct Cons<x, String<y...>> {typedef String<x, y...> value;};
-//template <char ... x, char y> using add = typename Ops<typename Cons<y, x...>::value>::rotated;
+template <typename T, char x> struct Add {};
+template <char x, char ... y> struct Add<String<y...>, x> {typedef String<y..., x> value;};
+
 
 
 int main() {
