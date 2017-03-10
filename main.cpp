501ba0edd6b7eed5ecd494e32fd78621a9af6fae
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 13:11:46 +0100

    asdfasdf

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template <char ... c> struct String {};
+template <typename T> struct Ops {};
+template <char x, char ... y> struct Ops<String<x, y...>> {static const char first = x; typedef String<y...> rest; typedef String<y..., x> rotated;};
+template <char x, typename T> struct Cons {};
+template <char x, char ... y> struct Cons<x, String<y...>> {typedef String<x, y...> value;};
+
+
+int main() {
+  Ops<String<'a', 'b'>> x;
+  std::cout << "Bra!" << std::endl;
+  return 0;
+}
