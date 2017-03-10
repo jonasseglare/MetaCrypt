908c2cadfd38cac2729f6395afa6067384bccbc6
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-03-10 23:08:47 +0100

    bra

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "metacrypt.h"
 
-td S<'R', 'E', '2'> Src; // <-- Put the data that you want to transform here.
+// Put the data that you want to transform here.
+td S<'R', 'E', '2'> Src; 
+
+// This is the encryption/decryption key (symmetric)
 td S<'K', 'A', 'T', 'T', 'S', 'K', 'I', 'T'> Key;
 
 // Use Md::Decode to decode, and Md::Encode to encode
+// Here we display the result
 tp <tn T> F The_message_is; The_message_is<tn Tr<Md::Decode, Key, Src>::v> x;
 
 int main() {
